2bc23f99b577feeccbc8052498b49fd3dfe665b2
JairoJonathanCernaRodriguez <113155501+JairoJonathanCernaRodriguez@users.noreply.github.com>
2024-11-10 16:25:21 -0600

    Error arreglado de Pantalla Details y multiples idiomas

--- app/src/main/java/com/example/inventory/ui/AppViewModelProvider.kt
+++ app/src/main/java/com/example/inventory/ui/AppViewModelProvider.kt
@@ -3,12 +3,14 @@ package com.example.inventory.ui
 import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.inventory.InventoryApplication
 import com.example.inventory.ui.item.NoteEntryViewModel
 import com.example.inventory.ui.item.NoteEditViewModel
+import com.example.inventory.ui.notes.NoteDetailsViewModel
 import com.example.inventory.ui.notes.NotesViewModel
 
 object AppViewModelProvider {
@@ -29,6 +31,14 @@ object AppViewModelProvider {
         initializer {
             NotesViewModel(
                 notesRepository = inventoryApplication().container.notesRepository
+            )
+        }
+        // Inicializar NoteDetailsViewModel con SavedStateHandle
+        initializer {
+            val savedStateHandle = this.createSavedStateHandle()  // Obtiene el SavedStateHandle
+            NoteDetailsViewModel(
+                notesRepository = inventoryApplication().container.notesRepository,
+                savedStateHandle = savedStateHandle
             )
         }
     }
